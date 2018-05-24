84c21f6f28cc51c160df4eb9f293df7422429678
valdecir.junior <valdecirpadovani.jr@gmail.com>
2018-05-24 17:24:25 -0300

    Commit 24/05

--- src/main/java/br/com/devmedia/curso/Service/CursoServiceImp.java
+++ src/main/java/br/com/devmedia/curso/Service/CursoServiceImp.java
@@ -1,38 +1,58 @@
 package br.com.devmedia.curso.Service;
 
+import br.com.devmedia.curso.dao.CursoDao;
 import br.com.devmedia.curso.domain.Curso;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
 
+
+@Service
+@Transactional
 public class CursoServiceImp implements CursoService{
+
+    @Autowired
+    private CursoDao dao;
+
     @Override
     public void save(Curso curso) {
-
+        dao.save(curso);
     }
 
     @Override
     public void update(Long id, Curso curso) {
-
+        curso.setId(id);
+        dao.update(curso);
     }
 
     @Override
     public void delete(Long id) {
-
+        dao.delete(id);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Curso findByid(Long id) {
-        return null;
+        return dao.findById(id);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Curso> findAll() {
-        return null;
+        return dao.findAll();
     }
 
     @Override
+    /*
+        Não precisa de usar o update do dao, pois o estado do objeto é Transiente
+        Nesse caso ao usar o setDataInicio, ja será feio o update no banco
+     */
     public Curso updateDataInicio(Long id, Date dataInicio) {
-        return null;
+        Curso curso = dao.findById(id);
+        curso.setDataInicio(dataInicio);
+        return curso;
     }
 }
